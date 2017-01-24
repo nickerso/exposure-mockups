2bc4d136a08bf73e634a56eeeabfea162af7b814
David Nickerson <nickerso@users.sourceforge.net>
2017-01-24 17:49:43 +1300

    Adding some intitial code to explore interaction with SVG diagrams.
    Currently just putting out console messages and alerts, but hopefully gives some idea of how to
    add interaction into SVG diagrams. Also not fully tested yet, but working using Chrome.

--- example-diagram/js/svgDiagram.js
+++ example-diagram/js/svgDiagram.js
@@ -19,12 +19,37 @@ define([
 	// passed in as subsequent arguments.
 
 	// global variables
-	var oldText = {};
+	var AllEntities = {};
+	
 
 	// This returned object becomes the defined value of this module
 	return {
+		// just print out a message to the console
 		message: function(msg) {
 			console.log(msg);
+		},
+		
+		// this will set up all the required callbacks
+		registerEntities: function(diagramObjectId, entities) {
+			var obj = dom.byId(diagramObjectId);
+			var doc = obj.getSVGDocument();
+			for (var name in entities)
+			{
+				var id = entities[name].id;
+				var svg = doc.getElementById(id);
+				svg.addEventListener("click", function(e) {
+					var entity = AllEntities[diagramObjectId][e.srcElement.id];
+					console.debug(entity);
+					alert("This would do something for clicking on the object: " + entity.id);
+				}, false);
+				svg.addEventListener("contextmenu", function(e) {
+					var entity = AllEntities[diagramObjectId][e.srcElement.id];
+					console.debug(entity);
+					alert("This would pop up a context menu for the object: " + entity.id);
+					// see https://dojotoolkit.org/reference-guide/1.10/dijit/Menu.html to get started...
+				}, false);
+			}
+			AllEntities[diagramObjectId] = entities;
 		}
 	};
 });
