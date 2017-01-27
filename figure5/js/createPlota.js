61c7ba61ba0875219cabcbe7bb5c3ae9151ab9c1
Jayden Long <jlon481@aucklanduni.ac.nz>
2017-01-27 14:09:46 +1300

    fxing 5a

--- figure5/js/createPlota.js
+++ figure5/js/createPlota.js
@@ -49,7 +49,7 @@ define([
 	
 	// set up the simulation data source
 	var simulationData = new CSV({
-		url: "/figure1/data/model5a.csv"
+		url: "/figure5/data/model5a.csv"
 	});
 	// and the data series for the potential data
 	var potentialSimulationData = new DataSeries(simulationData, {}, {
@@ -59,7 +59,7 @@ define([
 	
 	// set up temperature corrected experimental data 
 	var experimentalDataTemp = new CSV ({
-		url: "/figure1/data/exp5a.csv"
+		url: "/figure5/data/exp5a.csv"
 	})
 	// and the data series for the temperature corrected experimental data used in the paper
 	var potentialExperimental = new DataSeries(experimentalDataTemp, {}, {
