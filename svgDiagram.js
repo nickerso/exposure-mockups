4dea217f64a20a1dcdc3c48df66c421342a889d8
hflint97 <hfli678@aucklanduni.ac.nz>
2017-02-08 10:15:43 +1300

    Added new functions for previewing graphs
    The functionality changes are all contained in the svgDiagrams.js file. Essentially, a modal containing a picture and caption unique to each svg element will now pop up when the menu button labelled 'preview' is clicked. Further more, the toInfo function has been re-used to create the toGraph function, which essentially does the same thing, except it now jumps to images.
    The index.html file has had some images added to it to demonstrate the new functions, while the styles.css file has had code added to it that governs how the modal is displayed on the screen, as well as its sub-elements.

--- svgDiagram.js
+++ svgDiagram.js
@@ -3,6 +3,7 @@
  * - simulation data drawn with lines
  * - "experimental" data drawn with markers only and can have tooltips with the values
  */
+
 define([
 	// The dojo/dom module is required by this module, so it goes
 	// in this list of dependencies.
@@ -85,6 +86,7 @@ define([
 						var elementId=innerElement.getElementById(entity.id);
 						elementId.style.stroke=originalStroke;
 				});
+				
 				svg.addEventListener("click", function(e) {
 					var entity = AllEntities[diagramObjectId][e.srcElement.id];
 					console.debug(entity);
@@ -113,10 +115,11 @@ define([
 							
 						//create menu items
 							menu.addChild(new MenuItem({
-								/*This function below uses the name of an element in AllEntities
+								/*
+								  This function below uses the name of an element in AllEntities
 								  to call an element in the html document that has the same name.
 								*/
-								onClick: function(){
+								onClick: function toEquations(){
 									var tag = entity.equations
 									var htmlTag = document.getElementById(tag)
 									var info = htmlTag.getBoundingClientRect()
@@ -127,39 +130,58 @@ define([
 
 							menu.addChild(new MenuItem({
 								//This function essentially does the same thing as the one above. 
-								onClick: function(){
+								onClick: function toInfo(){
 									var tag = entity.info
 									var htmlTag = document.getElementById(tag)
 									var info = htmlTag.getBoundingClientRect()
-									window.scrollTo(0, info.top)	
-								
+									window.scrollTo(0, info.top)
 									},
-								
 								label: (entity.id+" information")
-							}));
-
+							}));							
+					
 							menu.addChild(new MenuItem({
-								//id: entityGraphs,
+								onClick: function(){alert("This would go to a graph relevant to the element selected")},
 								/*
-								  This function simply scrolls to a given point on the window.
-								  In this instance it goes to the top of the graph, but this
-								  could easily be modified so that tou could b taken to an 
-								  equation or paragraph somewhere else in the document. An issue 
-								  with this is that each important piece of information you want
-								  to link to must be created in a uniquely identified html element,
-								  which must then be put into the following function in the place
-								  of 'figure1GoesHere'. (see the function immediately above for 
-								  an example). This will also need a major overhaul to make it 
-								  far more general.
-								
-								
+								  This is pretty much a slightly reworked copy of toEquations and toInfo. 
+								  Functionally it is identical, it just references a different property
+								  of the entity selected.
+								*/
+								onClick: function toGraph(){
+									var tag = entity.graph
+									var htmlTag = document.getElementById(tag)
+									var info = htmlTag.getBoundingClientRect()
+									window.scrollTo(0, info.top)
+									},								
+								label: ("Graph for: "+entity.id)
+							}));
+							
+							menu.addChild(new MenuItem({
+								//This function creates a modal that can contain images specific
+								//to the svg element clicked. Essentially this creates an image 
+								//containing popup when the menu button is clicked.
 								onClick: function(){
-									var graphPos=figure1GoesHere.getBoundingClientRect()
-									window.scrollTo(0, graphPos.top)
-																
-								},*/
-								//onClick: function(){alert("The functionality for this isnt finished yet")},
-								label: (entity.id+": go to graph")
+									//This first section borrows heavily from toInfo, as it uses the 
+									//same sort of code in order to ensure that each svg element will
+									//link to its own unique graph.
+									var tag = entity.preview;
+									var htmlTag = document.getElementById(tag);
+									//This section is all new, based on the w3schools modal tutorial.
+									//Essentially it sets up a modal and places the correct image 
+									//inside it, while making the modal visible.
+									var modal = document.getElementById('myModal');
+									var modalImg = document.getElementById('img01');
+									var captionText = document.getElementById('caption');
+									modal.style.display = "block";
+									modalImg.src = htmlTag.src;
+									captionText.innerHTML = htmlTag.alt;
+									//This sets up the element that closes the modal
+									var closeButton = document.getElementsByClassName("close")[0];
+									// This function closes the modal when the close button is clicked
+									closeButton.onclick = function() { 
+										modal.style.display = "none";
+									}
+								},
+								label: (entity.id+" preview"),
 							}));
 	
 							menu.addChild(new MenuSeparator());
@@ -167,7 +189,7 @@ define([
 							var submenu = new Menu();
 							
 							submenu.addChild(new MenuItem({
-								label:("Other "+entity.id+" Action 1")
+								label:("Other "+membrane_graph+" Action 1")
 							}));
 							
 							submenu.addChild(new MenuItem({
@@ -180,23 +202,8 @@ define([
 							}));
 					
 							menu.placeAt("menus");
-							menu.startup();
-							
-							/*console.dir(menu);
-							menu.attachEvent("click", function(e){
-								if (MenuItem.label == entity.id+ " equations"){
-									alert("entityEquations was clicked")
-								} else if (MenuItem.label == entity.id+ " information"){
-									alert("entityInfo was clicked")
-								} else if (MenuItem.label == entity.id+" : go to graphs"){
-									("entityGraphs was clicked")
-								} else {
-									alert("nothing registered")
-								};
-							});*/
-						});
-						
-												
+							menu.startup();	
+						});							
 					//alert("This would pop up a context menu for the object: " + entity.id);
 					// see https://dojotoolkit.org/reference-guide/1.10/dijit/Menu.html to get started...
 				}, false);				
